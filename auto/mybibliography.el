054f92f4822f24ff92c8b66c9c694d2d5e208e20
Petko Rutesic <petko_rutesic@yahoo.com>
2023-07-31 10:31:27 +0200

    changed branching

---
+++ auto/mybibliography.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook
+ "mybibliography"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "lamy2017owlready"
+    "altinok2018ontology"
+    "hepp2008goodrelations"
+    "you2018graphrnn"
+    "petko2021enhancedMetamodel"
+    "rutesic2021enhanced"
+    "jin2018junction"
+    "kingma2013auto"
+    "agarwal2020review"
+    "al2011ontbot"
+    "wallace2009anatomy"
+    "ait2020kbot"
+    "turnip2019application"
+    "perez2020model"
+    "bocklisch2017rasa"))
+ '(or :bibtex :latex))
+
